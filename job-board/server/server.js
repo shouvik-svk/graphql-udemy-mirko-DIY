1c8e0524039c8bfd49f45210a54f13bcb3b1d8fe
Shouvik <32323183+shouvik-svk@users.noreply.github.com>
2022-05-31 12:25:20 +0530

    Section 3: Queries - Lesson 10: Apollo Server for Express

--- job-board/server/server.js
+++ job-board/server/server.js
@@ -1,8 +1,11 @@
+import { ApolloServer } from 'apollo-server-express';
 import cors from 'cors';
 import express from 'express';
 import { expressjwt } from 'express-jwt';
 import jwt from 'jsonwebtoken';
 import { User } from './db.js';
+import  { readFile } from 'fs/promises';
+import { resolvers } from './resolvers.js';
 
 const PORT = 9000;
 const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
@@ -19,11 +22,16 @@ app.post('/login', async (req, res) => {
   const user = await User.findOne((user) => user.email === email);
   if (user && user.password === password) {
     const token = jwt.sign({ sub: user.id }, JWT_SECRET);
-    res.json({ token });  
+    res.json({ token });
   } else {
     res.sendStatus(401);
   }
 });
+
+const typeDefs = await readFile('./schema.graphql', 'utf8');
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
