32a06c17e53dd97b06d44e986f4b8fbb54d3c856
Shouvik <32323183+shouvik-svk@users.noreply.github.com>
2022-06-01 15:55:42 +0530

    Section 3: Queries - Lesson 23: Error Handling

--- job-board/client/src/components/JobBoard.js
+++ job-board/client/src/components/JobBoard.js
@@ -4,11 +4,23 @@ import { useEffect, useState } from 'react';
 
 function JobBoard() {
   const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    getAllJobs().then((jobs) => setJobs(jobs));
+    getAllJobs()
+      .then((jobs) => setJobs(jobs))
+      .catch((error) => setError(true));
   }, []);
 
+  if(error) {
+    return (
+      <div>
+        <h1 className='title'>
+          An error occurred
+        </h1>
+      </div>
+    )
+  }
   return (
     <div>
       <h1 className="title">
