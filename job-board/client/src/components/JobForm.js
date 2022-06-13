f7641d656250728dac8f9585edcbe89e43b3ed54
Shouvik <32323183+shouvik-svk@users.noreply.github.com>
2022-06-13 11:34:05 +0530

    Section 7: Apollo React Hooks - Lesson 49: useCreateJob

--- job-board/client/src/components/JobForm.js
+++ job-board/client/src/components/JobForm.js
@@ -1,73 +1,57 @@
-import { useMutation } from '@apollo/client';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { getAccessToken } from '../auth';
-import { MUTATION_CREATE_JOB } from '../graphql/mutations';
-import { QUERY_GET_JOB_BY_ID } from '../graphql/queries';
+import { useCreateJob } from '../graphql/hooks';
 
 function JobForm() {
-  const navigate = useNavigate();
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [mutate] = useMutation(MUTATION_CREATE_JOB);
+   const navigate = useNavigate();
+   const [title, setTitle] = useState('');
+   const [description, setDescription] = useState('');
+   const { createJob, loading } = useCreateJob();
 
-  const handleSubmit = async (event) => {
+   const handleSubmit = async (event) => {
     event.preventDefault();
-    const { data: { job }} = await mutate({
-      variables: { createJobInput: { title, description}},
-      context: {
-        headers: { 'Authorization': `Bearer ${getAccessToken()}`}
-      },
-      update: (cache, { data: { job }}) => {
-        cache.writeQuery({
-          query: QUERY_GET_JOB_BY_ID,
-          variables: { id: job.id },
-          data: { job }
-        })
-      }
-    })
-    console.log('Job Created: ', job);
+    const job = await createJob(title, description);
     navigate(`/jobs/${job.id}`);
-  };
+   };
 
-  return (
-    <div>
-      <h1 className="title">
-        New Job
-      </h1>
-      <div className="box">
-        <form>
-          <div className="field">
-            <label className="label">
-              Title
-            </label>
-            <div className="control">
-              <input className="input" type="text" value={title}
-                onChange={(event) => setTitle(event.target.value)}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">
-              Description
-            </label>
-            <div className="control">
-              <textarea className="textarea" rows={10} value={description}
-                onChange={(event) => setDescription(event.target.value)}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <button className="button is-link" onClick={handleSubmit}>
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
+   return (
+      <div>
+         <h1 className="title">New Job</h1>
+         <div className="box">
+            <form>
+               <div className="field">
+                  <label className="label">Title</label>
+                  <div className="control">
+                     <input
+                        className="input"
+                        type="text"
+                        value={title}
+                        onChange={(event) => setTitle(event.target.value)}
+                     />
+                  </div>
+               </div>
+               <div className="field">
+                  <label className="label">Description</label>
+                  <div className="control">
+                     <textarea
+                        className="textarea"
+                        rows={10}
+                        value={description}
+                        onChange={(event) => setDescription(event.target.value)}
+                     />
+                  </div>
+               </div>
+               <div className="field">
+                  <div className="control">
+                     <button className="button is-link" disabled={loading} onClick={handleSubmit}>
+                        Submit
+                     </button>
+                  </div>
+               </div>
+            </form>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default JobForm;
--- job-board/client/src/graphql/hooks.js
+++ job-board/client/src/graphql/hooks.js
@@ -1,4 +1,6 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+import { getAccessToken } from "../auth";
+import { MUTATION_CREATE_JOB } from "./mutations";
 import { QUERY_GET_ALL_JOBS, QUERY_GET_COMPANY_BY_ID, QUERY_GET_JOB_BY_ID } from "./queries";
 
 export function useJobs() {
@@ -29,4 +31,30 @@ export function useCompany(id) {
     loading,
     error: Boolean(error),
   };
+}
+
+export function useCreateJob() {
+  const [mutate, { loading, error }] = useMutation(MUTATION_CREATE_JOB);
+  return {
+    createJob: async(title, description) => {
+      const {
+        data: { job },
+     } = await mutate({
+        variables: { createJobInput: { title, description } },
+        context: {
+           headers: { Authorization: `Bearer ${getAccessToken()}` },
+        },
+        update: (cache, { data: { job } }) => {
+           cache.writeQuery({
+              query: QUERY_GET_JOB_BY_ID,
+              variables: { id: job.id },
+              data: { job },
+           });
+        },
+     });
+     return job;
+    },
+    loading,
+    error: Boolean(error)
+  }
 }
